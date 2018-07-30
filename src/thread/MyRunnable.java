58ce21350d8a13404117eb99393e5f957e9b59fa
林英发 <530810890@qq.com>
2018-07-30 21:20:42 +0800

    线程

--- src/thread/MyRunnable.java
+++ src/thread/MyRunnable.java
@@ -1,14 +1,29 @@
 package thread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by xiaolin on 2018/7/25.
  */
 public class MyRunnable implements Runnable {
+
+    static AtomicInteger atomicInteger = new AtomicInteger(0);
+    int index = 0;
+
+    public MyRunnable() {
+        index = atomicInteger.getAndIncrement();
+    }
+
+    public MyRunnable(int index) {
+//        index = atomicInteger.getAndIncrement();
+        this.index = index;
+    }
+
     @Override
     public void run() {
-        System.out.println("我是Runnable");
+        System.out.println("我是Runnable  " + index);
         try {
-            Thread.sleep(5000);
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/thread/callable/TextExecutor.java
+++ src/thread/callable/TextExecutor.java
@@ -3,10 +3,7 @@ package thread.callable;
 import thread.MyRunnable;
 
 import java.util.ArrayDeque;
-import java.util.concurrent.Executor;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class TextExecutor {
 
@@ -17,7 +14,7 @@ public class TextExecutor {
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
                 2, Integer.MAX_VALUE,
                 60L, TimeUnit.SECONDS,
-                new SynchronousQueue<Runnable>());
+                new SynchronousQueue<Runnable>());//TODO SynchronousQueue 不保存任务runable
         threadPoolExecutor.allowCoreThreadTimeOut(true);
         THREAD_POOL_EXECUTOR = threadPoolExecutor;
     }
@@ -25,9 +22,15 @@ public class TextExecutor {
 
     public static void main(String[] args) {
         SerialExecutor serialExecutor = new SerialExecutor();
-        serialExecutor.execute(new MyRunnable());
-        serialExecutor.execute(new MyRunnable());
-        serialExecutor.execute(new MyRunnable());
+
+  /*      for (int i = 0; i < 100; i++) {
+            serialExecutor.submit(new MyRunnable(i));
+        }
+        serialExecutor.start();*/
+
+        for (int i = 0; i < 100; i++) {
+            serialExecutor.execute(new MyRunnable(i));
+        }
 
 
 //        Thread.currentThread().getName()=main
@@ -47,6 +50,29 @@ public class TextExecutor {
         final ArrayDeque<Runnable> mTasks = new ArrayDeque<Runnable>();
         private Runnable mActive;
 
+        BlockingQueue<Runnable> mT = new LinkedBlockingQueue<>();
+
+
+        public void submit(Runnable runnable) {
+            mT.add(runnable);
+        }
+
+        public synchronized void start() {
+            THREAD_POOL_EXECUTOR.execute(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            Runnable runnable = mT.take();
+                            runnable.run();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        }
+
         public synchronized void execute(final Runnable r) {
             mTasks.offer(new Runnable() {
                 public void run() {
@@ -55,18 +81,21 @@ public class TextExecutor {
                         //TODO 现在是在子线程，现在是模拟网络，睡眠5s，然后调用下一个线程，如此轮询
                         r.run();
                     } finally {
-                        //
+                        //TODO 一个线程执行完了，再执行下一个
                         scheduleNext();
                     }
                 }
             });
-
-            //TODO 首次执行
+//
+//            //TODO serialExecutor.execute(new MyRunnable());
+//            //TODO 首次执行，因为是在主线程调用提交任务，在子线程执行过程中是耗时的，可能会在主线程提交多个
+            //TODO  若线程执行中，就不再执行了
             if (mActive == null) {
                 scheduleNext();
             }
         }
 
+        //
         protected synchronized void scheduleNext() {
             if ((mActive = mTasks.poll()) != null) {
                 THREAD_POOL_EXECUTOR.execute(mActive);
